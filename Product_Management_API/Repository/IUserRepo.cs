fdda2fbf74ef5da5c10888bde745bc1d975fbc01
Amani <amanialshmali7@gmail.com>
2024-12-15 15:38:59 +0400

    add user Repo

---
+++ Product_Management_API/Repository/IUserRepo.cs
@@ -0,0 +1,10 @@
+﻿using Product_Management_API.Models;
+
+namespace Product_Management_API.Repository
+{
+    public interface IUserRepo
+    {
+        void AddUser(User user);
+        User GetUSer(string email, string password);
+    }
+}
---
+++ Product_Management_API/Repository/UserRepo.cs
@@ -0,0 +1,25 @@
+﻿using Product_Management_API.Models;
+
+namespace Product_Management_API.Repository
+{
+    public class UserRepo : IUserRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddUser(User user)
+        {
+            _context.users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public User GetUSer(string email, string password)
+        {
+            return _context.users.Where(u => u.Email == email & u.Password == password).FirstOrDefault();
+        }
+    }
+}
