26fdbd124be9d3a3339c4fc3eba2f1b133c3c0d4
Amani <amanialshmali7@gmail.com>
2024-12-15 15:43:09 +0400

    Add GenerateToken method

--- Product_Management_API/Program.cs
+++ Product_Management_API/Program.cs
@@ -1,7 +1,11 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Product_Management_API.Repository;
 using Product_Management_API.Service;
+using System.Text;
 
 namespace Product_Management_API
 {
@@ -17,14 +21,77 @@ namespace Product_Management_API
 
             // Register the repository and service for dependency injection
             builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
             builder.Services.AddScoped<IProductService, ProductService>();
+
+            builder.Services.AddScoped<IUserRepo, UserRepo>();
+
+            builder.Services.AddScoped<IUserService, UserService>();
 
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            // Add JWT Authentication
+            var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+            var secretKey = jwtSettings["SecretKey"];
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false, // You can set this to true if you want to validate the issuer.
+                    ValidateAudience = false, // You can set this to true if you want to validate the audience.
+                    ValidateLifetime = true, // Ensures the token hasn't expired.
+                    ValidateIssuerSigningKey = true, // Ensures the token is properly signed.
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)) // Match with your token generation key.
+                };
+            });
+
+
+
+
+
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer <token>')",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+            });
+
+
+
 
             var app = builder.Build();
 
