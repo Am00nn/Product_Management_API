9a3a74c4a66b8db743c2349dea2670603f7ab951
Amani <amanialshmali7@gmail.com>
2024-12-15 15:39:21 +0400

    add user service

---
+++ Product_Management_API/Service/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using Product_Management_API.Models;
+
+namespace Product_Management_API.Service
+{
+    public interface IUserService
+    {
+        void AddUser(User user);
+        User GetUser(string email, string password);
+    }
+}
---
+++ Product_Management_API/Service/UserService.cs
@@ -0,0 +1,29 @@
+﻿using Product_Management_API.Models;
+using Product_Management_API.Repository;
+
+namespace Product_Management_API.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepo _userrepo;
+
+        public UserService(IUserRepo userrepo)
+        {
+            _userrepo = userrepo;
+        }
+
+
+        public void AddUser(User user)
+        {
+
+            _userrepo.AddUser(user);
+        }
+
+        public User GetUser(string email, string password)
+        {
+            return _userrepo.GetUSer(email, password);
+
+        }
+
+    }
+}
