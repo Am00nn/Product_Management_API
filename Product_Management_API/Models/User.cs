fd0d1b90de0c231026c2f75d5e4955ccc8352c17
Amani <amanialshmali7@gmail.com>
2024-12-15 15:38:25 +0400

    add modle user

---
+++ Product_Management_API/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Product_Management_API.Models
+{
+    public class User
+    {
+        [Key]
+        [JsonIgnore]
+        public int UId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
